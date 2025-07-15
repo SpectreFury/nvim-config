8d362a60e8268369270db6a4f7e59c0d29073dd7
Ayush Soni <soni.ayush.2212@gmail.com>
2025-07-16 00:48:20 +0530

    Add: prettierd as conform formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,16 +4,25 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			go = { "goimports", "golines", "gofmt" },
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			json = { "prettierd" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+			scss = { "prettierd" },
+			markdown = { "prettierd" },
+			yaml = { "prettierd" },
 		},
 	},
-	keys = {
-		{
-			"<S-A-F>",
-			function()
-				require("conform").format({ async = true, lsp_format = "fallback" })
-			end,
-			mode = "",
-			desc = "[F]ormat buffer",
-		},
+	keys = { {
+		"<S-A-F>",
+		function()
+			require("conform").format({ async = true, lsp_format = "fallback" })
+		end,
+		mode = "",
+		desc = "[F]ormat buffer",
+	},
 	},
 }
