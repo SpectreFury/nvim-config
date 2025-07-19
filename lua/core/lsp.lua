52237f2011703c3f2225ece278889775a7272f7e
Ayush Soni <soni.ayush.2212@gmail.com>
2025-07-19 23:26:00 +0530

    Add: prisma

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({ "lua-ls", "gopls", "ts-ls", "clangd", "css-ls" })
+vim.lsp.enable({ "lua-ls", "gopls", "ts-ls", "clangd", "css-ls", "prisma" })
 
 vim.diagnostic.config({
 	virtual_text = true,
